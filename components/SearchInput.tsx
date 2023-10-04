c65c416630c9959ca9b87937680bdbcf760537e4
Rod Ritter <rritter@dvt.co.za>
2023-10-04 23:44:46 +0200

    tweak

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -13,7 +13,7 @@ import React, {
 } from "react";
 
 const SearchInput = () => {
-  const [word, setWord] = useState("");
+  const [word, setWord] = useState("keyboard");
   const [error, setError] = useState<string | null>(null);
   const { search } = useDictionary();
 
